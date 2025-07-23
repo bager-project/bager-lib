185751aeb2ea9dd12e1893d81028c13a16ed00b9
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2025-07-23 18:44:13 +0200

    L298N driver

--- src/bager_lib.h
+++ src/bager_lib.h
@@ -1,4 +1,8 @@
 #pragma once
 
-void init_led();
-void loop_led();
+void init_led(short pin);
+void turn_led_on_off(short pin, short on_off);
+
+void init_motors(short motor_one, short motor_two);
+void set_direction(short motor_one, short motor_two, short direction);
+void move_motors(short ena_pin);
--- src/main.c
+++ src/main.c
@@ -1,27 +1,50 @@
-#include "bager_lib.h"
+#include "hardware/pwm.h"
 #include "pico/stdlib.h"
 
-#define GPIO_OFF 0
-#define GPIO_ON 1
+#include "bager_lib.h"
 
-#define LED_PIN 25
-
-void init_led()
+void setup_pwm(uint gpio, uint16_t duty_cycle) 
 {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
+
+    uint slice = pwm_gpio_to_slice_num(gpio);
+    pwm_set_wrap(slice, 65535);  // Max count
+
+    pwm_set_chan_level(slice, pwm_gpio_to_channel(gpio), duty_cycle);
+    pwm_set_enabled(slice, true);
 }
 
-void loop_led()
+void init_led(short pin)
 {
-    while (1)
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_OUT);
+}
+
+void turn_led_on_off(short pin, short on_off)
+{
+    gpio_put(pin, on_off);
+}
+
+void init_motors(short motor_one, short motor_two)
+{
+    gpio_init(motor_one);
+    gpio_set_dir(motor_one, GPIO_OUT);
+
+    gpio_init(motor_two);
+    gpio_set_dir(motor_two, GPIO_OUT);
+}
+
+void set_direction(short motor_one, short motor_two, short direction)
+{
+    if (direction == 1) // forward
     {
-        gpio_put(LED_PIN, GPIO_ON);
-        sleep_ms(500);
-
-        gpio_put(LED_PIN, GPIO_OFF);
-        sleep_ms(500);
+        gpio_put(motor_one, 1);
+        gpio_put(motor_two, 0);
     }
+}
 
-    return;
+void move_motors(short ena_pin)
+{
+    // setup_pwm(ena_pin, 65535);  // 100% duty for max RPM
+    setup_pwm(ena_pin, 43690); // 6V
 }
